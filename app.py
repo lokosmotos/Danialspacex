45371ac228c5a18eb78753e30f033be5589bf544
lokosmotos <danial.khalif.dk@gmail.com>
2025-04-22 14:48:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
+from flask import Flask, render_template, request, redirect, send_file
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/cc-remover')
+def cc_remover():
+    return render_template('cc_remover.html')
+
+@app.route('/converter')
+def converter():
+    return render_template('converter.html')
+
+@app.route('/profanity-checker')
+def profanity_checker():
+    return render_template('profanity_checker.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
